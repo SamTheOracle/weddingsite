8d156140cdbb233431ea734b4bda687b902913d7
Giacomo Zanotti <gzano93@gmail.com>
2020-04-27 09:27:57 +0200

    minor

--- src/components/SecondSection.vue
+++ src/components/SecondSection.vue
@@ -2,7 +2,9 @@
   <v-container fluid class="pa-0" style="background-color:#F7F9E4">
     <v-row no-gutters align="start" justify="center">
       <v-col cols="6" v-if="$vuetify.breakpoint.smAndUp">
-        <v-img src="@/assets/description.png" style="height:98vh"/>
+      </v-col>
+      <v-col v-else>
+        <v-img src="@/assets/description.png"/>
       </v-col>
     </v-row>
   </v-container>

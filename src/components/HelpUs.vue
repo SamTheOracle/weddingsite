6f3e5ad15b392bc003aee89e162dc84264751ca8
Giacomo Zanotti <giacomo.zanotti@kalpa.it>
2020-08-05 13:02:45 +0200

    added name

--- src/components/HelpUs.vue
+++ src/components/HelpUs.vue
@@ -1,19 +1,20 @@
 <template>
   <v-container fluid style="background-color:#f7f9e4">
-    <v-row align="center" justify="center"  v-if="!swapLanguage">
-      <v-col  class="text-center">
+    <v-row align="center" justify="center" v-if="!swapLanguage">
+      <v-col class="text-center">
         <p class="nicetitle text-center">Un aiuto per il nostro futuro</p>
         <p class="descr text-center">Tanti piccoli sogni nel cassetto</p>
         <p class="overline mt-8" style="font-size:15px!important">DE42100110012626903491</p>
-        
+        <p class="text-caption">Giacomo Zanotti</p>
         <p class="descr text-center mt-8">Grazie</p>
       </v-col>
     </v-row>
     <v-row align="center" justify="center" v-else>
-      <v-col  class="text-center">
+      <v-col class="text-center">
         <p class="nicetitle text-center">Some help for our future</p>
         <p class="descr text-center">Many, little dreams to realize</p>
         <p class="overline mt-8" style="font-size:15px!important">DE42100110012626903491</p>
+        <p class="text-caption">Giacomo Zanotti</p>
         <p class="descr text-center mt-8">Thank you</p>
       </v-col>
     </v-row>
@@ -24,17 +25,16 @@
 export default {
   data: () => {
     return {
-      swapLanguage: false
-    }
+      swapLanguage: false,
+    };
   },
   props: {
-    language: String
+    language: String,
   },
   watch: {
     language: function () {
-      this.swapLanguage = !this.swapLanguage
-    }
-  }
-
-}
+      this.swapLanguage = !this.swapLanguage;
+    },
+  },
+};
 </script>

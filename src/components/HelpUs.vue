058db5c6faaca64ef04653852ad6dd8e69fe5263
Giacomo Zanotti <giacomo.zanotti@kalpa.it>
2020-08-05 13:01:21 +0200

    minor

--- src/components/HelpUs.vue
+++ src/components/HelpUs.vue
@@ -4,7 +4,8 @@
       <v-col  class="text-center">
         <p class="nicetitle text-center">Un aiuto per il nostro futuro</p>
         <p class="descr text-center">Tanti piccoli sogni nel cassetto</p>
-        <p class="overline mt-8" style="font-size:15px!important">IT59K0311111207000000000905</p>
+        <p class="overline mt-8" style="font-size:15px!important">DE42100110012626903491</p>
+        
         <p class="descr text-center mt-8">Grazie</p>
       </v-col>
     </v-row>
@@ -12,7 +13,7 @@
       <v-col  class="text-center">
         <p class="nicetitle text-center">Some help for our future</p>
         <p class="descr text-center">Many, little dreams to realize</p>
-        <p class="overline mt-8" style="font-size:15px!important">IT59K0311111207000000000905</p>
+        <p class="overline mt-8" style="font-size:15px!important">DE42100110012626903491</p>
         <p class="descr text-center mt-8">Thank you</p>
       </v-col>
     </v-row>

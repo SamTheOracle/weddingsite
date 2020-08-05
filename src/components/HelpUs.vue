f26cec9ced7a4d8f99cf0cc931092f29aa16a7e9
Giacomo Zanotti <giacomo.zanotti@kalpa.it>
2020-08-05 12:55:43 +0200

    minor

--- src/components/HelpUs.vue
+++ src/components/HelpUs.vue
@@ -3,7 +3,7 @@
     <v-row align="center" justify="center"  v-if="!swapLanguage">
       <v-col  class="text-center">
         <p class="nicetitle text-center">Un aiuto per il nostro futuro</p>
-        <p class="descr text-center">Breve frase ma intensa, che colpisce e ti lascia senza parole, rimanendo allo stesso tempo umile come noi</p>
+        <p class="descr text-center">Tanti piccoli sogni nel cassetto</p>
         <p class="overline mt-8" style="font-size:15px!important">IT59K0311111207000000000905</p>
         <p class="descr text-center mt-8">Grazie</p>
       </v-col>
@@ -11,7 +11,7 @@
     <v-row align="center" justify="center" v-else>
       <v-col  class="text-center">
         <p class="nicetitle text-center">Some help for our future</p>
-        <p class="descr text-center">This is a fairly short sentence in english, waiting for the italian to be defined such that i can be be useful</p>
+        <p class="descr text-center">Many, little dreams to realize</p>
         <p class="overline mt-8" style="font-size:15px!important">IT59K0311111207000000000905</p>
         <p class="descr text-center mt-8">Thank you</p>
       </v-col>

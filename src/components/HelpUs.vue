6127f0b0991a713efb49df86e326daab94cfc94a
Giacomo Zanotti <giacomo.zanotti@kalpa.it>
2020-08-06 16:01:18 +0200

    listanozze will be moved to next patch

--- src/components/HelpUs.vue
+++ src/components/HelpUs.vue
@@ -1,35 +1,23 @@
 <template>
   <v-container fluid style="background-color:#f7f9e4">
     <v-row align="center" justify="center" v-if="!swapLanguage">
-      <p class="nicetitle text-center">Un aiuto per il nostro futuro</p>
-    </v-row>
-    <v-row align="center" justify="center" v-else>
-      <p class="nicetitle text-center">Some help for our future</p>
-    </v-row>
-    <v-row align="center" justify="center" v-if="!swapLanguage">
       <v-col class="text-center">
-        <p class="descr text-center">Tanti, piccoli sogni nel cassetto da realizzare</p>
+        <p class="nicetitle text-center">Un aiuto per il nostro futuro</p>
+        <p class="descr text-center">Tanti piccoli sogni nel cassetto</p>
         <p class="overline mt-8" style="font-size:15px!important">DE42100110012626903491</p>
         <p class="text-caption">Giacomo Zanotti</p>
-      </v-col>
-      <v-col class="text-center">
-        <p class="descr text-center">Per la nostra casa</p>
-        <v-btn class="mt-6" color="#EBF0BA" rounded @click="openList()">Lista Nozze</v-btn>
+        <p class="descr text-center mt-8">Grazie</p>
       </v-col>
     </v-row>
     <v-row align="center" justify="center" v-else>
       <v-col class="text-center">
-        <p class="descr text-center">Many, small dreams to be realized</p>
+        <p class="nicetitle text-center">Some help for our future</p>
+        <p class="descr text-center">Many, little dreams to realize</p>
         <p class="overline mt-8" style="font-size:15px!important">DE42100110012626903491</p>
         <p class="text-caption">Giacomo Zanotti</p>
-      </v-col>
-      <v-col class="text-center">
-        <p class="descr text-center">For our home</p>
-        <v-btn class="mt-6" color="#EBF0BA" rounded @click="openList()">Wedding List</v-btn>
+        <p class="descr text-center mt-8">Thank you</p>
       </v-col>
     </v-row>
-    <p class="descr text-center mt-8" v-if="!swapLanguage">Grazie</p>
-    <p class="descr text-center mt-8" v-else>Thank You</p>
   </v-container>
 </template>
 
@@ -46,11 +34,6 @@ export default {
   watch: {
     language: function () {
       this.swapLanguage = !this.swapLanguage;
-    },
-  },
-  methods: {
-    openList() {
-      location.href = "https://www.amazon.it/wedding/share/giovannaegiacomo";
     },
   },
 };
